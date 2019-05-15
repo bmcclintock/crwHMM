37720b6ab3876ef17130cda774b2715fa430ed67
Devin <devin.johnson@noaa.gov>
2019-05-15 16:08:06 -0700

    added northernFurSeal to test file

--- testing/test1.R
+++ testing/test1.R
@@ -39,3 +39,20 @@ rbind(
 ) %>% `rownames<-`(c("crwHMM","crawl"))
 
 
+
+data("northernFurSeal")
+nfs <- northernFurSeal %>% st_as_sf(coords=c("long","lat")) %>% st_set_crs(4326) %>% 
+  st_transform(3832)
+nfs <- nfs %>% mutate(id="1") %>% rename(date=GMT, lc=loc_class) %>% 
+  select(id, date, lc, geometry)
+nfs <- prefilter(nfs)
+fit_crwHMM <- 
+  sfilter(
+    x = nfs,
+    time.step = 1,
+    fit.to.subset = FALSE,
+    optim="nlminb",
+    verbose = FALSE
+  )
+
+
