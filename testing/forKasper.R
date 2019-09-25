a86a4a5fafa7804c32e811fbcb731d9b6898cf99
Brett McClintock <brett.mcclintock@noaa.gov>
2019-09-24 17:32:50 -0700

    update reported number of non-zero elements when nbStates=2

--- testing/forKasper.R
+++ testing/forKasper.R
@@ -17,4 +17,4 @@ table(as.logical(fit1$ssm[[1]]$tmb$env$spHess(random=TRUE))==0) # 2360 non-zero 
 fit2 <- crwHMM::fit_ssm(subset(rope,id=="r11"), time.step = 24, nbStates=2, verbose=2)
 fit2$ssm[[1]]$tmb$env$spHess(random=TRUE) # has 0's instead of '.' in off-diagonal
 SparseM::image(fit2$ssm[[1]]$tmb$env$spHess(random=TRUE)) #look at covariance structure
-table(as.logical(fit2$ssm[[1]]$tmb$env$spHess(random=TRUE))==0) # 2634 non-zero elements out of 156816
+table(as.logical(fit2$ssm[[1]]$tmb$env$spHess(random=TRUE))==0) # 2822 non-zero elements out of 156816
